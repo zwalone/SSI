583912ae05ff913cab4512d46533207818b31743
Dariusz Momot <44140694+zwalone@users.noreply.github.com>
2020-03-25 21:16:57 +0100

    ex Bayesa

---
+++ TwierdzenieBayesa/Data.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TwierdzenieBayesa
+{
+    class Data
+    {
+        string[][] data;
+
+        public Data(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            data = new string[lines.Length][];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] temp = lines[i].Split(' ');
+                data[i] = new string[temp.Length];
+
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    data[i][j] = temp[j];
+                }
+            }
+        }
+
+        public void ChangeTemp(int index, double p1 , double p2 )
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                if(double.Parse(data[i][index]) <= p1)
+                {
+                    data[i][index] = "chlodno";
+                }
+                else if (double.Parse(data[i][index]) <= p2)
+                {
+                    data[i][index] = "cieplo";
+                }
+                else
+                {
+                    data[i][index] = "goraco";
+                }
+            }
+        }
+
+        public void PrintData()
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    Console.Write($"{data[i][j]}  ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public int getNumberOf(string s , int index)
+        {
+            int count = 0;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i][index] == s)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int getNumberOf(string s1 ,int indx1, string s2 , int indx2)
+        {
+            int count = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i][indx1] == s1 && data[i][indx2] == s2)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int Len { get { return data.Length; }}
+    }
+}
---
+++ TwierdzenieBayesa/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwierdzenieBayesa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = new string[3];
+            line[0] = "deszczowo";
+            line[1] = "goraco";
+            line[2] = "slaby";
+
+            Data data = new Data(@"C:\Users\Dariusz\Desktop\Studia\Rok2\Semestr 4\Systemy_Sztucznej_Inteligencji\Irysy\Bayesa.txt");
+            
+            data.ChangeTemp(1, 16, 20);
+            
+
+            int[] yes = new int[line.Length + 1];
+            int[] no = new int[line.Length + 1];
+
+            yes[0] = data.getNumberOf("tak", 3);
+            no[0] = data.getNumberOf("nie", 3);
+
+            //Get numbers
+            for (int i = 1; i < yes.Length; i++)
+            {
+                yes[i] = data.getNumberOf(line[i - 1], i - 1, "tak", 3);
+                no[i] = data.getNumberOf(line[i - 1], i - 1, "nie", 3);
+            }
+
+            double y = (double)yes[0] / (double)data.Len;
+            double n  = (double)no[0] / (double)data.Len;
+            for (int i = 1; i < yes.Length; i++)
+            {
+                y *= ((double)yes[i] / (double)yes[0]);
+                n *= ((double)no[i] / (double)no[0]);
+            }
+
+            if (y > n)
+            {
+                Console.Write("Decyzja : tak");
+            }
+            else
+            {
+                Console.Write("Decyzja : nie");
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
